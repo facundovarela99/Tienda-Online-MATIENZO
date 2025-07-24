51ac48c02a5a50175fd93bea9df3dffe9a1a2be3
facundovarela99 <facundovarela88@hotmail.com>
2025-07-23 22:47:13 -0300

    Reestructurando páginas de cada tipo de producto para hacer funcional la lógica básica de agregar elementos al contador de items del carrito en cada una de ellas.

--- js/cartShop.js
+++ js/cartShop.js
@@ -2,12 +2,18 @@ let item=0;
 let botones = document.querySelectorAll(".buttonAddToCart");
 let itemsCarrito = document.querySelector(".cantidadItemsCarrito");
 
+let totalMiniCarrito = document.querySelector(".totalMiniCarrito");
+let valores = document.querySelectorAll(".itemValue");
+let acumuladorTotal = 0;
+
 function addItemToCart(){
     itemsCarrito.innerHTML=(item+=1);
+    // totalMiniCarrito.innerHTML+=document.querySelector(".itemValue").innerHTML;
 }
 
-document.querySelector(".buttonAddToCart").addEventListener("click",addItemToCart);
+// document.querySelector(".buttonAddToCart").addEventListener("click",addItemToCart);
 
 botones.forEach(boton => {
     boton.addEventListener("click",addItemToCart);
 });
+
