cf09e809e64b56f1151106436a8e13dc47f90661
facundovarela99 <facundovarela88@hotmail.com>
2025-08-10 21:04:19 -0300

    Agregando funciones para sumar valores de productos tipo yerba al acumulador total de precios del carrito - modo testeo y práctica con JavaScript

--- js/cartShop.js
+++ js/cartShop.js
@@ -1,19 +1,63 @@
-let item=0;
-let botones = document.querySelectorAll(".buttonAddToCart");
+let item = 0;
 let itemsCarrito = document.querySelector(".cantidadItemsCarrito");
-
 let totalMiniCarrito = document.querySelector(".totalMiniCarrito");
-let valores = document.querySelectorAll(".itemValue");
 let acumuladorTotal = 0;
+let valorYerba1 = parseFloat(document.querySelector(".itemValue1").dataset.precioYerba1);
+let valorYerba2 = parseFloat(document.querySelector(".itemValue2").dataset.precioYerba2);
+let valorYerba3 = parseFloat(document.querySelector(".itemValue3").dataset.precioYerba3);
+let valorYerba4 = parseFloat(document.querySelector(".itemValue4").dataset.precioYerba4);
 
-function addItemToCart(){
+function agregarYerba1(){
+    acumuladorTotal+=valorYerba1;
     itemsCarrito.innerHTML=(item+=1);
-    // totalMiniCarrito.innerHTML+=document.querySelector(".itemValue").innerHTML;
+    totalMiniCarrito.innerHTML=`${acumuladorTotal}`;
 }
+function agregarYerba2(){
+    acumuladorTotal+=valorYerba2;
+    itemsCarrito.innerHTML=(item+=1);
+    totalMiniCarrito.innerHTML=`${acumuladorTotal}`;
+}
+function agregarYerba3(){
+    acumuladorTotal+=valorYerba3;
+    itemsCarrito.innerHTML=(item+=1);
+    totalMiniCarrito.innerHTML=`${acumuladorTotal}`;
+}
+function agregarYerba4(){
+    acumuladorTotal+=valorYerba4;
+    itemsCarrito.innerHTML=(item+=1);
+    totalMiniCarrito.innerHTML=`${acumuladorTotal}`;
+}
+
+document.querySelector(".buttonAddToCart1").addEventListener("click",agregarYerba1);
+document.querySelector(".buttonAddToCart2").addEventListener("click",agregarYerba2);
+document.querySelector(".buttonAddToCart3").addEventListener("click",agregarYerba3);
+document.querySelector(".buttonAddToCart4").addEventListener("click",agregarYerba4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let valores = document.querySelectorAll(".itemValue");
+// let botones = document.querySelectorAll(".buttonAddToCart");
+
+
+// function addItemToCart(){
+//     itemsCarrito.innerHTML=(item+=1);
+//     // totalMiniCarrito.innerHTML+=document.querySelector(".itemValue").innerHTML;
+// }
 
 // document.querySelector(".buttonAddToCart").addEventListener("click",addItemToCart);
 
-botones.forEach(boton => {
-    boton.addEventListener("click",addItemToCart);
-});
+// botones.forEach(boton => {
+//     boton.addEventListener("click",addItemToCart);
+// });
 
