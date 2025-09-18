4c6d016b9d4d81a5826cc22946b75f04e2b34619
Facundo Varela <facundovarela88@hotmail.com>
2025-09-18 12:04:25 -0300

    Commit de la lógica para agregar una cantidad especifica de productos al carrito en yerbas.js.

--- js/yerbas.js
+++ js/yerbas.js
@@ -57,7 +57,12 @@ function mostrarYerbas(yerbas) {
             <p class="fs-2 fw-bolder">${yerba.nombre}</p>
             ${renderDescripcion(yerba.descripcion)}
             <p class="fs-5 fw-normal ps-5">$ ${yerba.precio}.00</p>
-            <button class="button btnComprar${yerba.id} btn btn-dark buttonAddToCart" data-name="${yerba.nombre}" data-id="${yerba.id}" data-precio="${yerba.precio}" data-img="${yerba.imagen}" data-categoria="${yerba.categoria}">Agregar al carrito</button>
+            <hr>
+            <div class="inputCantidadYagregar d-flex flex-row align-items-center">
+                <label for="inputCantidad${yerba.id}" class="visually-hidden">Cantidad</label>
+                <input name="cantidadParaAgregar" id="inputCantidad${yerba.id}" class="inputCantidad" type="number" value="1" min="1">
+                <button class="button btnComprar${yerba.id} btn btn-dark buttonAddToCart" data-name="${yerba.nombre}" data-id="${yerba.id}" data-precio="${yerba.precio}" data-img="${yerba.imagen}" data-categoria="${yerba.categoria}">Agregar al carrito</button>
+            </div>
           </div>
         </div>
         `
@@ -79,6 +84,8 @@ function mostrarYerbas(yerbas) {
                 carrito = JSON.parse(localStorage.getItem('carrito'));
             }
 
+            const inputCantidad = document.getElementById(`inputCantidad${yerba.id}`);
+
             let nuevoProducto = {
                 'id': idYerba,
                 'nombre': nombreYerba,
@@ -86,18 +93,23 @@ function mostrarYerbas(yerbas) {
                 'imagen': imagenYerba,
                 'categoria':categoriaYerba
             };
-            carrito.push(nuevoProducto);
+
+            for (let i = 0; i < inputCantidad.value; i++) {
+                carrito.push(nuevoProducto);
+            }
+
             localStorage.setItem('carrito', JSON.stringify(carrito));
             let productosAlmacenados = Number(contadorElementosCarrito.textContent);
-            productosAlmacenados++;
             (localStorage.getItem('subTotalProductos') === null)
-            ? subTotal += parseInt(precioYerba)
-            : subTotal = parseInt(precioYerba)+Number(localStorage.getItem('subTotalProductos'))
+            ? subTotal += parseInt(precioYerba*inputCantidad.value)
+            : subTotal = parseInt(precioYerba*inputCantidad.value)+Number(localStorage.getItem('subTotalProductos'))
+            productosAlmacenados+=Number(inputCantidad.value);
             localStorage.setItem('contadorProductos', Number(productosAlmacenados));
             localStorage.setItem('subTotalProductos', Number(subTotal));
-            contadorElementosCarrito.innerHTML = localStorage.getItem('contadorProductos');
-            subTotalCarrito.innerHTML = localStorage.getItem('subTotalProductos');
-            swal(`¡${nombreYerba} agregada al carrito!`,"", "success");
+            contadorElementosCarrito.innerHTML = productosAlmacenados;
+            subTotalCarrito.innerHTML = subTotal;
+
+            swal(`¡ Se agregaron x${inputCantidad.value} ${nombreYerba} al carrito!`,"", "success");
         });
     });
 };
@@ -115,208 +127,3 @@ botonSideBar.addEventListener('click', abrirSidebar);
 botonSideBar.addEventListener('click',()=>{
     subTotal=0;
 })
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     let item = 0;
-//     let itemsCarrito = document.querySelector(".cantidadItemsCarrito");
-//     let totalMiniCarrito = document.querySelector(".totalMiniCarrito");
-//     let acumuladorTotal = 0;
-
-//     if (localStorage.getItem('itemsCarrito') === null && localStorage.getItem('totalMiniCarrito') === null) {
-//         itemsCarrito.innerHTML = 0;
-//         totalMiniCarrito.innerHTML = 0;
-//     } else {
-//         itemsCarrito.innerHTML = localStorage.getItem('itemsCarrito');
-//         totalMiniCarrito.innerHTML = localStorage.getItem('totalMiniCarrito');
-//     }
-
-//     const botonYerba1 = document.querySelector(".buttonAddYerbaToCart1");
-//     const botonYerba2 = document.querySelector(".buttonAddYerbaToCart2");
-//     const botonYerba3 = document.querySelector(".buttonAddYerbaToCart3");
-//     const botonYerba4 = document.querySelector(".buttonAddYerbaToCart4");
-
-//     if (botonYerba1) {
-//         let valorYerba1 = parseFloat(document.querySelector(".yerbaValue1").dataset.precioYerba1);
-//         botonYerba1.addEventListener('click', function () { //funcion agregarYerba1
-//             if (localStorage.getItem('itemsCarrito') === null || localStorage.getItem('itemsCarrito') === 0) {
-//                 acumuladorTotal += valorYerba1;
-//                 itemsCarrito.innerHTML = (item += 1);
-//                 totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//                 localStorage.setItem('itemsCarrito', Number(itemsCarrito.textContent))
-//                 localStorage.setItem('totalMiniCarrito', Number(totalMiniCarrito.textContent))
-//             } else {
-//                 acumuladorTotal += valorYerba1;
-//                 itemsCarrito.innerHTML = (item += 1);
-//                 totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//                 localStorage.setItem('itemsCarrito', Number(itemsCarrito.textContent))
-//                 localStorage.setItem('totalMiniCarrito', Number(totalMiniCarrito.textContent))
-//             }
-//         })
-//     }
-
-//     if (botonYerba2) {
-//         let valorYerba2 = parseFloat(document.querySelector(".yerbaValue2").dataset.precioYerba2);
-//         botonYerba2.addEventListener('click', function () { //funcion agregarYerba2
-//         //  if (localStorage.getItem('itemsCarrito') === null || localStorage.getItem('itemsCarrito') === 0) {
-//             acumuladorTotal += valorYerba2;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonYerba3) {
-//         let valorYerba3 = parseFloat(document.querySelector(".yerbaValue3").dataset.precioYerba3);
-//         botonYerba3.addEventListener('click', function () { //function agregarYerba3
-//             acumuladorTotal += valorYerba3;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonYerba4) {
-//         let valorYerba4 = parseFloat(document.querySelector(".yerbaValue4").dataset.precioYerba4);
-//         botonYerba4.addEventListener('click', function () { //funcion agregarYerba 4
-//             acumuladorTotal += valorYerba4;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     const botonTermo1 = document.querySelector(".buttonAddTermoToCart1");
-//     const botonTermo2 = document.querySelector(".buttonAddTermoToCart2");
-//     const botonTermo3 = document.querySelector(".buttonAddTermoToCart3");
-//     const botonTermo4 = document.querySelector(".buttonAddTermoToCart4");
-//     const botonTermo5 = document.querySelector(".buttonAddTermoToCart5");
-//     const botonTermo6 = document.querySelector(".buttonAddTermoToCart6");
-
-//     if (botonTermo1) {
-//         let valorTermo1 = parseFloat(document.querySelector(".termoValue1").dataset.precioTermo1);
-//         botonTermo1.addEventListener('click', function () {
-//             acumuladorTotal += valorTermo1;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonTermo2) {
-//         let valorTermo2 = parseFloat(document.querySelector(".termoValue2").dataset.precioTermo2);
-//         botonTermo2.addEventListener('click', function () {
-//             acumuladorTotal += valorTermo2;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonTermo3) {
-//         let valorTermo3 = parseFloat(document.querySelector(".termoValue3").dataset.precioTermo3);
-//         botonTermo3.addEventListener('click', function () {
-//             acumuladorTotal += valorTermo3;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonTermo4) {
-//         let valorTermo4 = parseFloat(document.querySelector(".termoValue4").dataset.precioTermo4);
-//         botonTermo4.addEventListener('click', function () {
-//             acumuladorTotal += valorTermo4;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonTermo5) {
-//         let valorTermo5 = parseFloat(document.querySelector(".termoValue5").dataset.precioTermo5);
-//         botonTermo5.addEventListener('click', function () {
-//             acumuladorTotal += valorTermo5;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonTermo6) {
-//         let valorTermo6 = parseFloat(document.querySelector(".termoValue6").dataset.precioTermo6);
-//         botonTermo6.addEventListener('click', function () {
-//             acumuladorTotal += valorTermo6;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     const botonMate1 = document.querySelector(".buttonAddMateToCart1");
-//     const botonMate2 = document.querySelector(".buttonAddMateToCart2");
-//     const botonMate3 = document.querySelector(".buttonAddMateToCart3");
-//     const botonMate4 = document.querySelector(".buttonAddMateToCart4");
-//     const botonMate5 = document.querySelector(".buttonAddMateToCart5");
-
-//     if (botonMate1) {
-//         let valorMate1 = parseFloat(document.querySelector('.mateValue1').dataset.precioMate1);
-//         botonMate1.addEventListener('click', function () {
-//             acumuladorTotal += valorMate1;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonMate2) {
-//         let valorMate2 = parseFloat(document.querySelector('.mateValue2').dataset.precioMate2);
-//         botonMate2.addEventListener('click', function () {
-//             acumuladorTotal += valorMate2;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonMate3) {
-//         let valorMate3 = parseFloat(document.querySelector('.mateValue3').dataset.precioMate3);
-//         botonMate3.addEventListener('click', function () {
-//             acumuladorTotal += valorMate3;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonMate4) {
-//         let valorMate4 = parseFloat(document.querySelector('.mateValue4').dataset.precioMate4);
-//         botonMate4.addEventListener('click', function () {
-//             acumuladorTotal += valorMate4;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-//     if (botonMate5) {
-//         let valorMate5 = parseFloat(document.querySelector('.mateValue5').dataset.precioMate5);
-//         botonMate5.addEventListener('click', function () {
-//             acumuladorTotal += valorMate5;
-//             itemsCarrito.innerHTML = (item += 1);
-//             totalMiniCarrito.innerHTML = `${acumuladorTotal}`;
-//         })
-//     }
-
-
-
-
-// });
-
-// let valores = document.querySelectorAll(".itemValue");
-// let botones = document.querySelectorAll(".buttonAddToCart");
-
-
-// function addItemToCart(){
-//     itemsCarrito.innerHTML=(item+=1);
-//     // totalMiniCarrito.innerHTML+=document.querySelector(".itemValue").innerHTML;
-// }
-
-// document.querySelector(".buttonAddToCart").addEventListener("click",addItemToCart);
-
-// botones.forEach(boton => {
-//     boton.addEventListener("click",addItemToCart);
-// });
-
